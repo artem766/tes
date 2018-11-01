ba36464344c941d870e5a6484ff82992fd6c2d3d
amubarak <amubarak@wooppay.com>
2018-11-01 16:02:44 +0600

    clear modiles

--- api/v1/config/routes.php
+++ api/v1/config/routes.php
@@ -8,11 +8,7 @@ return [
 			'vendor/yii2module/yii2-account/src/api/v2',
 			//'vendor/yii2module/yii2-profile/src/api/v2',
 			'vendor/yii2lab/yii2-geo/src/api',
-			'vendor/yii2woop/yii2-service/src/api/v3',
 			'vendor/yii2module/yii2-summary/src/api',
-			'vendor/yii2woop/yii2-history/src/api',
-			'vendor/yii2woop/yii2-bank/src/api/v2',
-			'vendor/yii2woop/yii2-operation/src/api/v2',
 		],
 	],
 	
--- frontend/config/modules.php
+++ frontend/config/modules.php
@@ -8,7 +8,6 @@ $config = [
 	/*'profile' => 'yii2module\profile\module\v1\Module',*/
 	'welcome' => 'yii2module\dashboard\web\Module',
 	'guide' => 'yii2module\guide\module\Module',
-	/*'article' => 'yii2module\article\web\Module',*/
 ];
 
 if(YII_ENV_DEV) {
