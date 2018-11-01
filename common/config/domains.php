a931d2726085f6814b30057f26954b908a4a5a0b
amubarak <amubarak@wooppay.com>
2018-11-01 15:11:25 +0600

    clear packages

--- common/config/domains.php
+++ common/config/domains.php
@@ -41,19 +41,5 @@ return [
 	'rbac' => 'yii2lab\rbac\domain\Domain',
 	'lang' => 'yii2module\lang\domain\Domain',
 	'geo' => 'yii2lab\geo\domain\Domain',
-	'service' => 'yii2woop\service\domain\v3\Domain',
-	'summary' => 'yii2module\summary\domain\Domain',
-	'operation' => 'yii2woop\operation\domain\v2\Domain',
-	'history' => 'yii2woop\history\domain\Domain',
-	'bank' => 'yii2woop\bank\domain\v2\Domain',
 	'jwt' => 'yii2lab\extension\jwt\Domain',
-	'partner' => [
-		'class' => 'yii2woop\partner\domain\Domain',
-		'services' => [
-			'info' => [
-				'partnerId' => 8,
-				'jwtProfile' => 'core',
-			],
-		],
-	],
 ];
